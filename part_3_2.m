f6da85b4baa22a108e0d7556296983020555d1f3
Mathew Hanson <mthanson2004@gmail.com>
2025-11-17 14:02:05 -0500

    Initial commit of time constant extraction from step response

--- part_3_2.m
+++ part_3_2.m
@@ -2,7 +2,7 @@ clc; clear; close all;
 
 baseDir = fileparts(mfilename('fullpath'));
 
-% List all voltage subfolders (e.g., '1_0V', '1_5V')
+%% List all voltage subfolders (e.g., '1_0V', '1_5V')
 voltageFolders = dir(fullfile(baseDir,'Lab3_Group6_Data/2-5/', '*V'));
 voltageFolders = voltageFolders([voltageFolders.isdir]);  % keep only folders
 
@@ -13,7 +13,7 @@ alpha = 0.54; % =m2/m1
 beta = 0.1; % =b2/b1
 gamma = 0.1; % =d2/d1
 
-% Filter Velocity Data to Find Vss
+%% Filter Velocity Data to Find Vss
 for i = 1:length(voltageFolders)
     vFolderName = voltageFolders(i).name;
     vFolderPath = fullfile(baseDir, 'Lab3_Group6_Data/2-5/', vFolderName);
@@ -41,7 +41,7 @@ for i = 1:length(voltageFolders)
         end
     end
 
-    % Divide by number of files to get average
+    %% Divide by number of files to get average
     for k = 1:length(fields)
         field = fields{k};
         if ~isempty(meanData.(field))
@@ -52,44 +52,40 @@ for i = 1:length(voltageFolders)
     voltageStr = strrep(vFolderName, '_', '.');
     u0(i) = str2double(voltageStr(1:end-1));
 
+    %% Process data 
     fs = 1000;
+    total_time = 8.0;
 
-    % crop first 20% of data to remove transients
+    % mask for b/d work
+    mask_bd = makeMask(meanData.v1_filt, fs, total_time,...
+                        0.25, ... % intial fraction cut (25%)
+                        1.0,  ... % original win_start
+                        0.5,  ... % win_length
+                        1.0);     % period
+    
+    % mask for m1 work
+    mask_m1 = makeMask(meanData.v1_filt, fs, total_time,...
+                        0.25, ... % intial fraction cut (25%)
+                        1.5,  ... % original win_start
+                        0.5,  ... % win_length
+                        1.0);     % period
+
+    % mask for m2 work
+    mask_m2 = makeMask(meanData.v1_filt, fs, total_time,...
+                        0.25, ... % intial fraction cut (25%)
+                        1.5,  ... % original win_start
+                        0.5,  ... % win_length
+                        1.0);     % period
+
+    % crop first 25% of data to remove transients
     initial_cut = 0.25 * 8; % =1.6s
     samples_cut = initial_cut * fs;
     v1_trim = meanData.v1_filt(samples_cut+1:end);
-    v2_trim = meanData.v2_filt(samples_cut+1:end);
+    v2_trim = meanData.v2_filt(samples_cut+1:end); 
 
-    % remove periodic 0.5s to isolate ss velocity
-    original_win_start = 1.0;
-    win_length = 0.5;
-    period = 1.0;
-    total_time = 8.0;
-
-    % convert to trimmed-time frame
-    trim_win_start = original_win_start - initial_cut;   
-    trim_period = period;
-    trim_win_length = win_length;
-
-    mask = true(size(v1_trim));
-
-    t = trim_win_start;
-    while t < (total_time - initial_cut)
-        if t >= 0
-            startSample = floor(t * fs) + 1;
-            endSample   = floor((t + trim_win_length) * fs);
-
-            startSample = max(startSample, 1);
-            endSample   = min(endSample, length(v1_trim));
-
-            mask(startSample:endSample) = false;
-        end
-        t = t + trim_period;
-    end
-
-    % Compute d_t/b_t from Vss
-    v1_clean = v1_trim(mask);
-    v2_clean = v2_trim(mask);
+    %% Compute d_t/b_t from Vss
+    v1_clean = v1_trim(mask_bd);
+    v2_clean = v2_trim(mask_bd);
 
     den = max([abs(v1_clean), abs(v2_clean)], [], 2);
     percent_diff = abs(v1_clean - v2_clean) ./ den;
@@ -108,8 +104,66 @@ for i = 1:length(voltageFolders)
 
     Vss(i) = avg_v;
 
+    %% Compute m1 for give u0
+    peak_period = 500; %peaks repeat every 500 samples in cleaned data
 
+    m1 = abs(v1_trim(mask_m1));
+    m1 = m1(1:end-1); % convert from 3001 -> 3000 entities
+    m1_sum = reshape(m1, peak_period, []);
+    m1_avg = mean(m1_sum,2);
 
+    %m1_0 = m1_avg(1);
+    m1_max = max(m1_avg);
+    m1_targ = 0.63*m1_max;
+
+    t_start_m1 = 0;
+    t_time_c_m1 = 0;
+
+    for j = 2:length(m1_avg)
+        
+        if m1_avg(j) > 0.01 && t_start_m1 == 0
+            t_start_m1 = j;
+        end
+
+        if abs(m1_avg(j) - m1_targ) <= 0.1 * m1_targ
+            t_time_c_m1 = j;
+            break;
+        end
+    end
+    
+    tau_m1(i) = t_time_c_m1-t_start_m1;
+
+    %% Compute m2 only for u_0=1.5V
+    if u0(i) == 1.5
+        start_trim = 100; % trim first 100ms to remove transients
+        m2_pre = abs(v2_trim(mask_m2));
+        m2_pre = m2_pre(start_trim:end-1);
+
+        % make sample size a multiple of period:
+        end_trim = mod(length(m2_pre),peak_period);
+        m2 = m2_pre(1:end-end_trim);
+
+        m2_sum = reshape(m2, peak_period, []);
+        m2_avg = mean(m2_sum, 2);
+
+        m2_max = max(m2_avg);
+        m2_targ = 0.63 * m2_max;
+
+        t_start_m2 = 0;
+        t_time_c_m2 = 0;
+
+        for j = 2:length(m2_avg)
+            if m2_avg(j) > 0.01 && t_start_m2 == 0
+                t_start_m2 = j;
+            end
+
+            if abs(m2_avg(j) - m2_targ) <= 0.1 * m2_targ
+                t_time_c_m2 = j;
+                break;
+            end
+        end
+        tau_m2 = t_time_c_m2 - t_start_m2;
+    end
 end
 
 
@@ -121,3 +175,37 @@ b_t = x(1);
 d_t = x(2);
 
 
+function mask = makeMask(v1, fs, total_time, initial_cut_frac, ...
+                         original_win_start, win_length, period)
+
+    % --- Crop first X% of data (initial transient removal) ---
+    initial_cut = initial_cut_frac * total_time;      % seconds
+    samples_cut = round(initial_cut * fs);
+
+    v1_trim = v1(samples_cut+1:end);   % only used for sizing mask
+    N = length(v1_trim);
+
+    % --- Convert timing values into trimmed reference frame ---
+    trim_win_start  = original_win_start - initial_cut;
+    trim_win_length = win_length;
+    trim_period     = period;
+
+    % --- Initialize mask (default = keep data) ---
+    mask = true(N,1);
+
+    % --- Apply periodic window removal ---
+    t = trim_win_start;
+
+    while t < (total_time - initial_cut)
+        if t >= 0
+            startSample = floor(t * fs) + 1;
+            endSample   = floor((t + trim_win_length) * fs);
+            % Bounds check
+            startSample = max(startSample, 1);
+            endSample   = min(endSample, N);
+
+            mask(startSample:endSample) = false;
+        end
+        t = t + trim_period;
+    end
+end
